c0c7cdea89e42ad24c58d9b8675ffb958372357b
Fernando Ayats <ayatsfer@gmail.com>
2024-04-20 19:49:51 +0200

    cleanup functions

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use nix::sched::{clone, unshare, CloneFlags};
 use nix::sys::prctl::set_pdeathsig;
 use nix::sys::signal::Signal;
 use nix::sys::wait::{waitpid, WaitStatus};
-use nix::unistd::{close, getgid, getpid, getuid, Gid, Uid};
+use nix::unistd::{close, getgid, getpid, getuid, Gid, Pid, Uid};
+use once_cell::sync::OnceCell;
 use std::ffi::OsString;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -31,53 +32,39 @@ struct Args {
     command: Vec<String>,
 }
 
+#[derive(Debug, Clone)]
+struct Config {
+    target: PathBuf,
+    runtime: PathBuf,
+    cache: PathBuf,
+    allocation: String,
+    layer: PathBuf,
+    _work: PathBuf, // Implementation of overlayfs
+    uid: Uid,
+    gid: Gid,
+}
+
 fn main() -> eyre::Result<()> {
-    color_eyre::install()?;
     {
+        color_eyre::install()?;
         use tracing_subscriber::{fmt, prelude::*, EnvFilter};
         tracing_subscriber::registry()
             .with(fmt::layer().without_time().with_line_number(true))
             .with(EnvFilter::from_default_env())
             .init();
+
+        if env::var("HOVER").is_ok() {
+            bail!("hover can't be stacked!");
+        };
+        env::set_var("HOVER", "1");
     }
 
     let args = <Args as clap::Parser>::parse();
     debug!(?args);
 
-    if env::var("HOVER").is_ok() {
-        bail!("hover can't be stacked!");
-    };
+    let config = Config::build()?;
 
-    let uid = getuid();
-    let gid = getgid();
-    let pid = getpid();
-    debug!(?uid, ?gid, ?pid);
-
-    ensure!(!uid.is_root(), "hover-rs is not made to be run as root!");
-
-    let app_cache = env::var("XDG_CACHE_HOME")
-        .map(PathBuf::from)
-        .unwrap_or_else(|_| env::var("HOME").map(PathBuf::from).unwrap().join(".cache"))
-        .join("hover-rs");
-    std::fs::create_dir_all(&app_cache)?;
-
-    let allocation = {
-        let format = time::format_description::parse("[year]-[month]-[day]-[hour][minute]")?;
-        let now: time::OffsetDateTime = SystemTime::now().into();
-        let mytime = now.format(&format)?;
-        use rand::Rng;
-        let seed: String = rand::thread_rng()
-            .sample_iter(&rand::distributions::Alphanumeric)
-            .take(7)
-            .map(char::from)
-            .collect();
-        format!("{mytime}-{seed}")
-    };
-
-    let app_runtime = env::var("XDG_RUNTIME_DIR")
-        .map(|s| PathBuf::from(s).join("hover-rs"))
-        .unwrap_or_else(|_| PathBuf::from("/tmp").join(format!("hover-rs-{allocation}")));
-    std::fs::create_dir_all(&app_runtime)?;
+    ensure!(!config.uid.is_root(), "hover-rs is not made to be run as root!");
 
     let (argv0, argv) = if args.command.is_empty() {
         (env::var("SHELL").ok().unwrap_or(String::from("sh")), vec![])
@@ -100,6 +87,7 @@ fn main() -> eyre::Result<()> {
 
     let mut stack = [0; 4000];
     let child = unsafe {
+        let config = config.clone();
         clone(
             Box::new(move || {
                 callback_wrapper(|| -> eyre::Result<()> {
@@ -112,7 +100,7 @@ fn main() -> eyre::Result<()> {
                     debug!("Parent done");
 
                     set_pdeathsig(Some(Signal::SIGTERM))?;
-                    slave(&app_runtime, &app_cache, &allocation, uid, gid)?;
+                    setup(&config)?;
                     let error = cmd.exec();
                     Err(error).wrap_err("Failed to execute the command")
                 })
@@ -138,7 +126,7 @@ fn main() -> eyre::Result<()> {
             .read(true)
             .write(true)
             .open(format!("/proc/{child}/uid_map"))?;
-        let msg = format!("0 {uid} 1");
+        let msg = format!("0 {} 1", config.uid);
         f.write(msg.as_bytes())
             .wrap_err("Setting uid_map for child process")?;
     }
@@ -155,7 +143,7 @@ fn main() -> eyre::Result<()> {
             .read(true)
             .write(true)
             .open(format!("/proc/{child}/gid_map"))?;
-        let msg = format!("0 {gid} 1");
+        let msg = format!("0 {} 1", config.gid);
         f.write(msg.as_bytes())
             .wrap_err("Setting gid_map for child process")?;
     }
@@ -173,28 +161,26 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-fn slave(
-    app_runtime: &Path,
-    app_cache: &Path,
-    allocation: &str,
-    uid: Uid,
-    gid: Gid,
-) -> eyre::Result<()> {
-    let target = PathBuf::from(env::var("HOME")?);
-
+fn setup(config: &Config) -> eyre::Result<()> {
     mount(
         Some("tmpfs"),
-        app_runtime,
+        &config.runtime,
         Some("tmpfs"),
         MsFlags::empty(),
         NNONE,
     )?;
 
-    let ro_mount = app_runtime.join("oldroot");
+    let ro_mount = config.runtime.join("oldroot");
     fs::create_dir_all(&ro_mount)?;
 
     // Mount root dir as RO
-    mount(Some(&target), &ro_mount, NNONE, MsFlags::MS_BIND, NNONE)?;
+    mount(
+        Some(&config.target),
+        &ro_mount,
+        NNONE,
+        MsFlags::MS_BIND,
+        NNONE,
+    )?;
     mount(
         NNONE,
         &ro_mount,
@@ -203,23 +189,18 @@ fn slave(
         NNONE,
     )?;
 
-    let layer_dir = app_cache.join(format!("layer-{allocation}"));
-    fs::create_dir_all(&layer_dir)?;
-    let work_dir = app_cache.join(format!(".work-{allocation}"));
-    fs::create_dir_all(&work_dir)?;
-
     {
         // Don't use format! because the paths might not be valid str, keep OsStr's
         let mut opts = OsString::from("lowerdir=");
         opts.push(ro_mount.as_os_str());
         opts.push(",upperdir=");
-        opts.push(layer_dir.as_os_str());
+        opts.push(&config.layer);
         opts.push(",workdir=");
-        opts.push(work_dir);
+        opts.push(&config._work);
 
         mount(
             Some("overlay"),
-            &target,
+            &config.target,
             Some("overlay"),
             MsFlags::empty(),
             Some(opts.as_os_str()),
@@ -232,7 +213,7 @@ fn slave(
     // Seal the working dirs from the user
     mount(
         Some("/var/empty"),
-        app_runtime,
+        &config.runtime,
         NNONE,
         MsFlags::MS_BIND,
         NNONE,
@@ -240,7 +221,7 @@ fn slave(
 
     mount(
         Some("/var/empty"),
-        app_cache,
+        &config.cache,
         NNONE,
         MsFlags::MS_BIND,
         NNONE,
@@ -254,7 +235,7 @@ fn slave(
             .read(true)
             .write(true)
             .open("/proc/self/uid_map")?;
-        let msg = format!("{uid} 0 1");
+        let msg = format!("{} 0 1", config.uid);
         f.write(msg.as_bytes())?;
     }
     {
@@ -262,7 +243,7 @@ fn slave(
             .read(true)
             .write(true)
             .open("/proc/self/gid_map")?;
-        let msg = format!("{gid} 0 1");
+        let msg = format!("{} 0 1", config.gid);
         f.write(msg.as_bytes())?;
     }
 
@@ -271,15 +252,57 @@ fn slave(
         println!("You are now {}", "hovering~".bold());
         println!(
             "  A layer is covering your {}",
-            target.to_string_lossy().bold().red()
+            config.target.to_string_lossy().bold().red()
         );
         println!(
             "  You will find the leftovers at: {}",
-            layer_dir.to_string_lossy().bold().red()
+            config.layer.to_string_lossy().bold().red()
         );
     }
 
-    env::set_var("HOVER", "1");
-
     Ok(())
+}
+
+impl Config {
+    fn build() -> eyre::Result<Self> {
+        let cache = env::var("XDG_CACHE_HOME")
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| env::var("HOME").map(PathBuf::from).unwrap().join(".cache"))
+            .join("hover-rs");
+        std::fs::create_dir_all(&cache)?;
+
+        let allocation = {
+            let format = time::format_description::parse("[year]-[month]-[day]-[hour][minute]")?;
+            let now: time::OffsetDateTime = SystemTime::now().into();
+            let mytime = now.format(&format)?;
+            use rand::Rng;
+            let seed: String = rand::thread_rng()
+                .sample_iter(&rand::distributions::Alphanumeric)
+                .take(7)
+                .map(char::from)
+                .collect();
+            format!("{mytime}-{seed}")
+        };
+
+        let runtime = env::var("XDG_RUNTIME_DIR")
+            .map(|s| PathBuf::from(s).join("hover-rs"))
+            .unwrap_or_else(|_| PathBuf::from("/tmp").join(format!("hover-rs-{allocation}")));
+        std::fs::create_dir_all(&runtime)?;
+
+        let layer = cache.join(format!("layer-{allocation}"));
+        fs::create_dir_all(&layer)?;
+        let _work = cache.join(format!(".work-{allocation}"));
+        fs::create_dir_all(&_work)?;
+
+        Ok(Self {
+            target: PathBuf::from(env::var("HOME")?),
+            runtime,
+            cache,
+            allocation,
+            layer,
+            _work,
+            uid: Uid::current(),
+            gid: Gid::current(),
+        })
+    }
 }
